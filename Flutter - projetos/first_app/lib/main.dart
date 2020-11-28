de0a089a3feab87f4202acbf7dd49961cea9e0ba
Oberdan Costa dos Santos <oberdan@agidesk.com.br>
2020-11-28 09:24:58 -0300

    My First App

---
+++ Flutter - projetos/first_app/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'My First Flutter app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _calc = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: GestureDetector(
+        onLongPress: () {
+          _calc = 1;
+          setState(() {});
+        },
+        onTap: () {
+          _calc *= 2;
+          setState(() {});
+        },
+        child: Text(
+          'calc : $_calc',
+          style: TextStyle(
+            fontSize: 28,
+          ),
+        ),
+      )),
+    );
+  }
+}
